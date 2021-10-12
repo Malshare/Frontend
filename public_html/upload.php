a62cf1f095a2cc2d2224cd818378d5bb4fc0cd94
Silas Cutler <silas.cutler@blacklistthisdomain.com>
2021-10-12 13:24:49 -0400

    Update upload.php
    updated upload size limit

--- public_html/upload.php
+++ public_html/upload.php
@@ -2,7 +2,7 @@
 $SUCCESS = false;
 $RESULTS = "";
 if( (array_key_exists( 'fsample', $_FILES ) && ($_FILES['fsample'])) ){
-	if ($_FILES["fsample"]["size"] > 10000000) {
+	if ($_FILES["fsample"]["size"] > 26214400) {
 		$RESULTS = "File too Large: <i> 10Mb Max</i>";
 	}
 	else{

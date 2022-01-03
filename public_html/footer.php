6a8ccf980cef0df75a249472a025010af7638309
Silas Cutler <silas@blacklab.io>
2022-01-03 15:32:59 -0500

    Set footer to dynamically adjust year:

--- public_html/footer.php
+++ public_html/footer.php
@@ -3,7 +3,7 @@
 
 	<div id="footer">
 		<div class="container text-center">
-			<p class="credit">	&copy; 2012 - 2021 MalShare  | 
+			<p class="credit">	&copy; 2012 - <?php echo date("Y"); ?> MalShare  | 
 				<a href="tos.php"> Terms of Service </a> | 
 				<a href="sitemap.php"> Sitemap</a> | 
 				<a href="https://twitter.com/mal_share?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @mal_share</a><script async src="//platform.twitter.com/widgets.js"></script>

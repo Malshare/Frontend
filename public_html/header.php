f71ea4c87a8e0ba42b72c836fd3c9889db3ae5c1
Silas Cutler <silas.cutler@gmail.com>
2024-06-07 09:55:24 -0400

    Update header.php

--- public_html/header.php
+++ public_html/header.php
@@ -8,17 +8,13 @@
 	<link href="./css/sticky-footer-navbar.css" rel="stylesheet">
 	<link href="./css/popup.css" rel="stylesheet">
 
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-4WK6RGYZEY"></script>
 	<script>
-
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-49931431-1']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'G-4WK6RGYZEY');
 	</script>
 
